fffd3854f9436f5bf5c8abc2c6a817d6388074ad
Pekorachi2 <hajh2004@gmail.com>
2020-12-07 00:56:01 +0900

    bot upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'Nzg1MDU2NjU0MzkwOTg0NzI1.X8yS_A.JsKUJzs-WidKBxp3-C29Ky8l04M';
+const token = process.env.token;
 
 client.on('ready', () => {
     console.log('켰다.');
